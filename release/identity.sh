fb11d831eb6d55d7329a3ce4b51d53876f7a8032
Chad Gauthier <chad.gauthier@crowdstreet.com>
2023-01-17 20:50:17 -0600

    update identity

--- release/identity.sh
+++ release/identity.sh
@@ -1,4 +1,4 @@
-export GIT_COMMITTER_NAME="Dragonpilot Team"
-export GIT_COMMITTER_EMAIL="dgnpilot@gmail.com"
-export GIT_AUTHOR_NAME="Dragonpilot Team"
-export GIT_AUTHOR_EMAIL="dgnpilot@gmail.com"
+export GIT_COMMITTER_NAME="Chad Gauthier"
+export GIT_COMMITTER_EMAIL="chadgauth@gmail.com"
+export GIT_AUTHOR_NAME="Zenpilot Team"
+export GIT_AUTHOR_EMAIL="chadgauth@gmail.com"
